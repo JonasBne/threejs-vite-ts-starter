2da5352743f20f55caf681e94edd991888d00ad0
Jonas <boone.jonas@outlook.be>
2022-10-23 16:37:15 +0200

    feat: added placeholder elements to render a basic scene

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,27 @@
 import './style.css'
 import * as THREE from 'three';
 
-console.log(THREE)
+// initiate a scene
+const scene = new THREE.Scene();
+
+// basic mesh
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00})
+const mesh = new THREE.Mesh(geometry, material);
+
+const sizes = {
+    height: 600,
+    width: 600,
+}
+
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 1, 1000);
+camera.position.z = 3;
+
+// add mesh and camera to the scene
+scene.add(mesh, camera);
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.getElementsByClassName('webgl')[0]
+});
+
+renderer.render(scene, camera);
