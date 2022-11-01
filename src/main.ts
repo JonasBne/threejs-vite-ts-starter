4679d0c338cecf6b73b59f7a49a97da3cf3b02b3
Jonas <boone.jonas@outlook.be>
2022-11-01 20:08:36 +0100

    refactor: change default camera settings

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
 // camera
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 3;
 scene.add(camera);
 
