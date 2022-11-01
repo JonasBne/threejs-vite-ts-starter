633d024d6c7e80e16c94945d99b4a9ebedf18bf9
Jonas <boone.jonas@outlook.be>
2022-11-01 20:05:59 +0100

    feat: add orbit controls

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import './style.css'
 import * as THREE from 'three';
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+
+// canvas
+const canvas = document.getElementsByClassName('webgl')[0] as HTMLCanvasElement;
 
 // initiate a scene
 const scene = new THREE.Scene();
@@ -8,17 +12,31 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00})
 const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
+// camera
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
 camera.position.z = 3;
+scene.add(camera);
 
-// add mesh and camera to the scene
-scene.add(mesh, camera);
+// controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
+// renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: document.getElementsByClassName('webgl')[0]
+    canvas
 });
 // add size to renderer to avoid pixelated view
 renderer.setSize(window.innerWidth, window.innerHeight)
 
-renderer.render(scene, camera);
+const animate = () => {
+    // enable damping
+    controls.update()
+    // render scene
+    renderer.render(scene, camera);
+    // pass reference to itself to create infinite loop of frames
+    window.requestAnimationFrame(animate);
+}
+
+animate();
